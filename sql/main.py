9ee2cd06f9a23eb5b9406e2dd0d7f5445c28c1e6
vinh2vinh <vinh7302000@gmail.com>
2023-08-25 21:56:24 -0700

    Update main.py

--- sql/main.py
+++ sql/main.py
@@ -1,8 +1,36 @@
 import sqlite3
+<<<<<<< Updated upstream
 
 
 
 
+=======
+import csv #adds csv library to read csv files
+conn = sqlite3.connect('metals.db') #Creates database called gold
+cursor = conn.cursor() #apparently you need this to execute SQL commands
+cursor.execute('''CREATE TABLE IF NOT EXISTS Gold (
+                    Date,
+                    Close_Last,
+                    Volume,
+                    Open,
+                    High,
+                    Low
+                 )''') #first sql commands make table called gold, collums will be csv categories
+#r is a prefix that goes before file paths, i'm a dumbass
+temp = cursor.execute('''
+Select *
+From Gold
+;
+''')
+rows = cursor.fetchall() #singular row in the CSV
+i = 0 #fuckin ittorator
+while (i < 10):
+    print(rows[i])
+    i=i+1
+
+conn.commit() #commits all the shit into itself and makes it permanant 
+conn.close() #kills itself
+>>>>>>> Stashed changes
 
 
 
